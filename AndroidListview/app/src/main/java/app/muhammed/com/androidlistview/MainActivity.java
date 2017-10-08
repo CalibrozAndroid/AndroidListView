046021ba9832f7bc3895aa33da2590a9e6f85ae1
Muhammed Thasneem P Android <muhammed.thasneem@yahoo.com>
2017-10-08 15:10:51 +0530

    Item click remove, Item click, item long press

--- AndroidListview/app/src/main/java/app/muhammed/com/androidlistview/MainActivity.java
+++ AndroidListview/app/src/main/java/app/muhammed/com/androidlistview/MainActivity.java
@@ -1,8 +1,10 @@
 package app.muhammed.com.androidlistview;
 
 
+import android.content.DialogInterface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.ListViewCompat;
 import android.view.View;
@@ -14,9 +16,11 @@ import java.util.ArrayList;
 
 import app.muhammed.com.androidlistview.model.ContactModel;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     private ListViewCompat mContactListViewCompat;
+    private ArrayList<String> strings;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +39,15 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         mContactListViewCompat = (ListViewCompat) findViewById(R.id.contactListView);
         mContactListViewCompat.setDivider(new ColorDrawable(getResources().getColor(R.color.colorAccent)));
         mContactListViewCompat.setDividerHeight(1);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, getContactString());
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, getContactString());
 
 
         mContactListViewCompat.setAdapter(arrayAdapter);
 
 
         mContactListViewCompat.setOnItemClickListener(this);
+
+        mContactListViewCompat.setOnItemLongClickListener(this);
 
 
     }
@@ -80,7 +86,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     public ArrayList<String> getContactString() {
 
-        ArrayList<String> strings = new ArrayList<>();
+        strings = new ArrayList<>();
 
         strings.add("Muhammed");
         strings.add("Muhammed");
@@ -105,10 +111,50 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-        String itemAtPosition = (String) adapterView.getItemAtPosition(i);
+//        String itemAtPosition = (String) adapterView.getItemAtPosition(i);
 
-        Toast.makeText(this, itemAtPosition + " pos : " + i, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, itemAtPosition + " pos : " + i, Toast.LENGTH_SHORT).show();
 
 
+//        showDialogHere(i);
+
+
+    }
+
+    private void showDialogHere(final int position) {
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+
+        builder.setTitle("Delete action");
+
+        builder.setMessage("Are you sure you want to delete?");
+
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                strings.remove(position);
+
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", null);
+
+
+
+        builder.create().show();
+
+
+
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+        showDialogHere(i);
+        return false;
     }
 }
